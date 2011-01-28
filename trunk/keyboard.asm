f01132943069867c3abeb864d1bd5f7803cdf663
supportprimis@gmail.com <supportprimis@gmail.com@9cf4d4dd-9137-1fd2-4056-d6c225d89a22>
2011-01-28 02:25:45 +0000

    Commented the keyboard interrupt, now you know whats going on.

--- trunk/keyboard.asm
+++ trunk/keyboard.asm
@@ -77,92 +77,92 @@ kb_handler:                          ; not a dummy function :3
     je .shift_u                      ; Jump to shift
    cmp al, 0xAA                      ; Is shift up?
     je .shift_u                      ; Jump to shift
-   cmp al, 0x1D                      ; 
-    je .ctrl_d                       ;
-   cmp al, 0x9D                      ;
-    je .ctrl_u                       ;
-   cmp al, 0x38                      ;
-    je .alt_d                        ;
-   cmp al, 0xB8                      ;
-    je .alt_u                        ;
-   cmp al, 0x3A                      ;
-    je .caps_d                       ;
-   cmp al, 0x45                      ;
-    je .num_d                        ;
-   cmp al, 0x46                      ;
-    je .scrl_d                       ;
-   cmp al, 0xBA                      ;
-    je .done                         ;
-   cmp al, 0xC5                      ;
-    je .done                         ;
-   cmp al, 0xC6                      ;
-    je .done                         ;
-   cmp al, 0xE0                      ;
-    je .done                         ;
-   jmp .compare                      ;
-.shift_d:                            ;
-    or bl, 10000000b                 ;
-    mov byte [spec_key], bl          ;
-    jmp .done                        ;
-.shift_u:                            ;
-    and bl, 01111111b                ;
-    mov byte [spec_key], bl          ;
-    jmp .done                        ;
-.ctrl_d:                             ;
-    or bl, 01000000b                 ;
-    mov byte [spec_key], bl          ;
-    jmp .done                        ;
-.ctrl_u:                             ;
-    and bl, 10111111b                ;
-    mov byte [spec_key], bl          ;
-    jmp .done                        ;
-.alt_u:                              ;
-    or bl, 00100000b                 ;
-    mov byte [spec_key], bl          ;
-    jmp .done                        ;
-.alt_d:                              ;
-    and bl, 11011111b                ;
-    mov byte [spec_key], bl          ;
-    jmp .done                        ;
-.caps_d:                             ;
-    not byte [caps_lock]             ;
-    call .update_led                 ;
-    jmp .done                        ;
-.num_d:                              ;
-    not byte [num_lock]              ;
-    call .update_led                 ;
-    jmp .done                        ;
-.scrl_d:                             ;
-    not byte [scrl_lock]             ;
-    call .update_led                 ;
-    jmp .done                        ;
-.update_led:                         ;
-    xor bl, bl                       ;
-    mov cl, byte [caps_lock]         ;
-    and cl, 0x04                     ;
-    add bl, cl                       ;
-    mov cl, byte [num_lock]          ;
-    and cl, 0x02                     ;
-    add bl, cl                       ;
-    mov cl, byte [scrl_lock]         ;
-    and cl, 0x01                     ;
-    add bl, cl                       ;
-    call .keyboardloop               ;
-    mov al, 0xED                     ;
-    out 0x60, al                     ;
-    call .keyboardloop               ;
-    mov al, bl                       ;
-    out 0x60, al                     ;
-    ret                              ;
-.keyboardloop:                       ;
-    push ax                          ;
-    in al, 0x64                      ;
-    and al, 2                        ;
-    cmp al, 0                        ;
-    ja .keyboardloop                 ;
-    pop ax                           ;
-    ret                              ;
-;------------------------------------;
+   cmp al, 0x1D                      ; Ctrl down
+    je .ctrl_d                       ; Jump to the code
+   cmp al, 0x9D                      ; Ctrl up
+    je .ctrl_u                       ; Jump to the code
+   cmp al, 0x38                      ; Alt Pressed
+    je .alt_d                        ; Jump to the code
+   cmp al, 0xB8                      ; Alt Released
+    je .alt_u                        ; Jump to the code
+   cmp al, 0x3A                      ; Caps Lock Down
+    je .caps_d                       ; Jump to the code
+   cmp al, 0x45                      ; Number Lock Down
+    je .num_d                        ; Jump to the code
+   cmp al, 0x46                      ; Scroll Lock Down
+    je .scrl_d                       ; Jump to the code
+   cmp al, 0xBA                      ; Caps Lock released
+    je .done                         ; Skip to the end
+   cmp al, 0xC5                      ; Number Lock released
+    je .done                         ; Skip to the end
+   cmp al, 0xC6                      ; Scroll Lock was released
+    je .done                         ; Skip to the end
+   cmp al, 0xE0                      ; The first code to the right alt and ctrl
+    je .done                         ; Skip to the end
+   jmp .compare                      ; If nothing was changed, we go back to our normally scheduled programming (yes that is the correct terms :D)
+.shift_d:                            ; This is shift down code
+    or bl, 10000000b                 ; We OR the shift bit here
+    mov byte [spec_key], bl          ; Then we put it back to the variable
+    jmp .done                        ; Jump to end of interrupt.
+.shift_u:                            ; Shift Up Code
+    and bl, 01111111b                ; We AND bl with every bit except the shift one
+    mov byte [spec_key], bl          ; Then we put bl in the special key var
+    jmp .done                        ; Jump to end of interrupt.
+.ctrl_d:                             ; Ctrl down
+    or bl, 01000000b                 ; Same as shift, cept we move it over one
+    mov byte [spec_key], bl          ; How about a poem to pass the lines?
+    jmp .done                        ; I found a pretty bookmark
+.ctrl_u:                             ; As sweet as it can be
+    and bl, 10111111b                ; Was in the middle of the park
+    mov byte [spec_key], bl          ; And on the ground you see!
+    jmp .done                        ; Whenever I am reading
+.alt_u:                              ; It only wants to show
+    or bl, 00100000b                 ; Exactly where I’ve been
+    mov byte [spec_key], bl          ; And where I want to go!
+    jmp .done                        ; Don’t touch my little bookmark
+.alt_d:                              ; Or take it from its home
+    and bl, 11011111b                ; For I will only read again
+    mov byte [spec_key], bl          ; The pages that I know
+    jmp .done                        ; Yup.
+.caps_d:                             ; Still got more lines to kill
+    not byte [caps_lock]             ; How about some computer related quotes?  
+    call .update_led                 ; "Never trust a computer you can't throw out a window."
+    jmp .done                        ;  - Steve Wozniak
+.num_d:                              ; "Hardware: The parts of a computer system that can be kicked."
+    not byte [num_lock]              ;  - Jeff Pesis
+    call .update_led                 ; "There are two major products that come out of Berkeley: LSD and BSD.
+    jmp .done                        ; We don't believe this to be a coincidence."
+.scrl_d:                             ;  - Jeremy S. Anderson
+    not byte [scrl_lock]             ; "Good code is its own best documentation."
+    call .update_led                 ;  - Steve McConnell
+    jmp .done                        ; Ok Back to reality here;
+.update_led:                         ; Update the keyboard LED's
+    xor bl, bl                       ; First we make bl 0
+    mov cl, byte [caps_lock]         ; Then we move the caps lock to cl
+    and cl, 0x04                     ; AND it with 0x04 to leave only one bit on (third bit)
+    add bl, cl                       ; If anything is left, it gets added to bl
+    mov cl, byte [num_lock]          ; Now the same thing with num_lock
+    and cl, 0x02                     ; 0x02 with Number Lock (second bit)
+    add bl, cl                       ; Belive it or not, the update_led routine is very fast.
+    mov cl, byte [scrl_lock]         ; Only 30 clock cycles when keyboardloop isn't blocked.
+    and cl, 0x01                     ; Can be slower of course...
+    add bl, cl                       ; Worst case is about 50 cycles waiting for the keyboard controller.
+    call .keyboardloop               ; Now we request the keyboard controller
+    mov al, 0xED                     ; Here we tell it "Yo! we have status bits for your led's!"
+    out 0x60, al                     ; We send it to the chip here
+    call .keyboardloop               ; We wait for it to understand us, its for precautionary sake
+    mov al, bl                       ; Here we move the status bits to al to be sent
+    out 0x60, al                     ; And lastly, we send it down the line to the keyboard controller
+    ret                              ; Then we return.
+.keyboardloop:                       ; This subfunction waits for the keyboard controller to be ready
+    push ax                          ; We use ax in this function, but we also need it in the parent function
+    in al, 0x64                      ; We request the keyboards status bits here
+    and al, 2                        ; Second bit is basically the keyboard saying "I'm busy"
+    cmp al, 0                        ; We check if it's set
+    ja .keyboardloop                 ; If so, we just loop again.
+    pop ax                           ; Otherwise, we're done, get out of here
+    ret                              ; Return to parent function
+;------------------------------------; Hey! thats the end of the keyboard interrupt! now it may seem like it would take forever to run, but its a lot slower in the BIOS!
 
 ;-----[ get a charecter from input ]--------------;
 getchar:                                          ; Input nothing, Output al = charecter
